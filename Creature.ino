b91ead5a30bf5659debe71d532d27bc40a8debc0
Richin <richin29@gmail.com>
2018-09-16 11:41:37 +0200

    First Version

--- Creature.ino
+++ Creature.ino
@@ -1,8 +1,11 @@
+/************************** INCLUDES **************************/
 #include "Servo.h"
 
-unsigned char current_angle = INIT_ANGLE;
-Servo creature_servo;
+/************************** VARIABLES *************************/
+static unsigned char current_angle = INIT_ANGLE;
+static Servo creature_servo;
 
+/************************** FUNCTIONS *************************/
 void CreateCreature( void )
 {
   creature_servo.attach(SERVO_PIN);
@@ -10,16 +13,16 @@ void CreateCreature( void )
 
 void TurnOffSwitch( void )
 {
-  for(current_angle; current_angle < 180; current_angle += 5)
+  for(current_angle; current_angle <= 180; current_angle += 5)
   {                                  
     creature_servo.write(current_angle);
-    delay(10);
+    delay(5);
   } 
 }
 
 void GetBackInside( void )
 {
-  for(current_angle; current_angle>=1; current_angle-=5)
+  for(current_angle; current_angle>=INIT_ANGLE; current_angle-=5)
   {                                
     creature_servo.write(current_angle);
     delay(5);                       
--- UselessBox.ino
+++ UselessBox.ino
@@ -2,30 +2,29 @@
 
 /************************** INCLUDES **************************/
 
-/************************** VARIABLES *************************/
-#define SERVO_PIN ((unsigned char) 3)
-#define INIT_ANGLE ((unsigned short int) 30)
+/************************** DEFINES ***************************/
+#define BUTTON_PIN   ((unsigned char) 2)
+#define SERVO_PIN    ((unsigned char) 3)
+#define INIT_ANGLE   ((unsigned short int) 60)
 
-int button_gpio = 2;
-int servo_pin = 3;
-int led_gpio = LED_BUILTIN;
+/************************** VARIABLES *************************/
 int button_val;
 
 /************************** FUNCTIONS *************************/
 void setup() 
 {
-  pinMode(button_gpio, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   CreateCreature();
 } 
   
 void loop() 
-{
-  
-  TurnOffSwitch(); 
- 
-  delay(1000);
-  button_val = digitalRead(button_gpio);     // read the input pin
-  digitalWrite(led_gpio, button_val);    // sets the LED to the button's value
-  GetBackInside();
-  delay(1000);
+{ 
+  button_val = digitalRead(BUTTON_PIN);
+  if (button_val == LOW) {
+    TurnOffSwitch();
+    delay(100);
+  }
+  else {
+    GetBackInside();
+  }
 }
